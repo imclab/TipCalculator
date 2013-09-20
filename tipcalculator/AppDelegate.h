c9368ccaf74016b77e679bc8bed199b563fe95a5
Eric <Ericrius1@gmail.com>
2013-09-20 15:13:03 -0700

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by eric levin on 9/20/13.
+//  Copyright (c) 2013 eric levin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculatorTests/tipcalculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  tipcalculatorTests.h
+//  tipcalculatorTests
+//
+//  Created by eric levin on 9/20/13.
+//  Copyright (c) 2013 eric levin. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface tipcalculatorTests : SenTestCase
+
+@end
