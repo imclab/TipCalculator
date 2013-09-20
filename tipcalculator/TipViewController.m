e7929374f311dfb5a6bd8b9b1268439125367fbf
Eric <Ericrius1@gmail.com>
2013-09-20 16:09:42 -0700

    hooking up events

--- tipcalculator/TipViewController.m
+++ tipcalculator/TipViewController.m
@@ -10,6 +10,18 @@
 
 @interface TipViewController ()
 
+//Allows you to access elements
+@property (weak, nonatomic) IBOutlet UITextField *billTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+
+//'-' indicated instance and not class method
+//the return type is inside first parenthesis
+//Param type is inside second parenthesis
+- (IBAction)onTap:(id)sender;
+-(void)updateValues;
+
 @end
 
 @implementation TipViewController
@@ -35,4 +47,23 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:YES];
+    [self updateValues];
+}
+
+- (void)updateValues {
+    //coverts string to float
+    float billAmount = [self.billTextField.text floatValue];
+    
+    NSArray *tipValues = @[@(0.1), @(0.15), @(0.2)];
+    
+    float tipAmount = billAmount *  [tipValues[self.tipControl.selectedSegmentIndex] floatValue];
+    
+    float totalAmount = tipAmount + billAmount;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%0.2f", tipAmount];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%0.2f", totalAmount];
+    
+}
 @end
